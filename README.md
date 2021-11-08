da8441ee41d9e239e24e30dfd71bd6089f552bc1
Jannis Mohlin Tsiroyannis <jannis.mohlintsiroyannis@kb.se>
2021-11-08 14:19:13 +0100

    Initial commit. Just standard scaffolding.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Unpaywall mirror
+
+To "ingest" a dump do the following:
+
+´zcat unpaywall_snapshot_2021-07-02T151134.jsonl.gz | head -10000 | split -l 128 --filter='gzip > $FILE.gz' - /tmp/splittest/´
---
+++ src/main/java/unpaywall/Api.java
@@ -0,0 +1,17 @@
+package unpaywall;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Api extends HttpServlet
+{
+    public void init()
+    {
+    }
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+    {
+    }
+}
